9e68b4b0c4794a84ddf0d82da554c08493ce945c
andrewye0128 <b0936486128@gmail.com>
2026-08-31 07:14:18 +0800

    chore: initialize ASP.NET Core MVC project

---
+++ README.md
@@ -0,0 +1,74 @@
+\# VenueGo
+
+
+
+球館預約與訂單管理系統。
+
+
+
+\## 專案介紹
+
+
+
+VenueGo 是以 ASP.NET Core MVC 開發的球館預約後台管理系統，
+
+提供管理員進行球館、場地、會員、預約、訂單、付款與退款等資料管理。
+
+
+
+\## 技術架構
+
+
+
+\- ASP.NET Core MVC
+
+\- .NET 10
+
+\- Entity Framework Core
+
+\- SQL Server
+
+\- Bootstrap 5
+
+\- jQuery
+
+\- Git
+
+\- GitHub
+
+
+
+\## 開發環境
+
+
+
+\- Visual Studio
+
+\- .NET 10 SDK
+
+\- SQL Server
+
+\- SQL Server Management Studio
+
+
+
+\## 專案架構
+
+
+
+```text
+
+VenueGo
+
+├── Controllers
+
+├── Models
+
+├── Views
+
+├── wwwroot
+
+├── appsettings.json
+
+└── Program.cs
+
---
+++ VenueGo/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using VenueGo.Models;
+
+namespace VenueGo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
